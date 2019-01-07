20cd5041a57adc17f1adc8a7dfd61f20d34c5060
Robin Kahlow <robinkahlow@web.de>
2019-01-07 14:32:15 +0000

    added script for converting images + segmentation image to tfrecords

---
+++ image_segmentation/color_to_tfrecord.py
@@ -0,0 +1,43 @@
+import io
+import os
+from glob import glob
+
+import numpy as np
+import tensorflow as tf
+from imageio import imread
+from tqdm import tqdm
+
+from image_segmentation import build_data
+
+def image_array_to_bytes(array):
+    byte_buffer = io.BytesIO()
+    array.save(byte_buffer, format="png")
+    byte_buffer.seek(0)
+    return byte_buffer.read()
+
+def get_segmentation_indices(path):
+    img = imread(path)
+    index_img = np.zeros((img.shape[0], img.shape[1], 3), dtype=np.uint8)
+    index_img[img[:, :, 0] > 0, 0] = 1
+    return image_array_to_bytes(index_img)
+
+def main():
+    width, height = 512, 512
+    color_paths = glob("/home/joel/datasets/ade_mrpt/*_C.png")
+    seg_paths = glob("/home/joel/datasets/ade_mrpt/*_S.png")
+
+    assert len(color_paths) == len(seg_paths)
+
+    with tf.python_io.TFRecordWriter("dataset.tfrecords") as tfrecord_writer:
+        for color_path, seg_path in tqdm(zip(color_paths, seg_paths), total=len(color_paths)):
+            image_data = tf.gfile.FastGFile(color_path, "rb").read()
+            seg_data = get_segmentation_indices(seg_path)
+
+            example = build_data.image_seg_to_tfexample(
+                    image_data, color_path, height, width, seg_data)
+            tfrecord_writer.write(example.SerializeToString())
+
+    print("Done")
+
+if __name__ == "__main__":
+    main()
