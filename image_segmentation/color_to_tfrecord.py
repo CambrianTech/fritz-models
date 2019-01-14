a772132966678238079ab48b184724b8aaf58ca0
Robin Kahlow <robinkahlow@web.de>
2019-01-14 18:20:31 +0000

    randomly shuffle paths when writing to tfrecords

--- image_segmentation/color_to_tfrecord.py
+++ image_segmentation/color_to_tfrecord.py
@@ -6,6 +6,7 @@ import numpy as np
 import tensorflow as tf
 from imageio import imread, imwrite
 from tqdm import tqdm
+import random
 
 from image_segmentation import build_data
 
@@ -28,8 +29,12 @@ def main():
 
     assert len(color_paths) == len(seg_paths)
 
+    # Shuffle the paths randomly
+    paths = list(zip(color_paths, seg_paths))
+    random.shuffle(paths)
+    
     with tf.python_io.TFRecordWriter("dataset.tfrecords") as tfrecord_writer:
-        for color_path, seg_path in tqdm(zip(color_paths, seg_paths), total=len(color_paths)):
+        for color_path, seg_path in tqdm(paths):
             image_data = tf.gfile.FastGFile(color_path, "rb").read()
             seg_data = get_segmentation_indices(seg_path)
 
